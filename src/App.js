6220081086bb259b1edf263fb50d7a75c63e0318
Troy Dorman <troydorman@gmail.com>
2022-08-01 19:51:21 -0600

    text to speech works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,115 @@
 import "./App.css";
+const logo = require("./img/speech.png");
 
 function App() {
+  // init speech synthesis API
+  const synth = window.speechSynthesis;
+
+  // DOM element reference vars
+  const textForm = document.querySelector("form");
+  const textInput = document.querySelector("#text-input");
+  const voiceSelect = document.querySelector("#voice-select");
+  const rate = document.querySelector("#rate");
+  const rateValue = document.querySelector("#rate-value");
+  const pitch = document.querySelector("#pitch");
+  const pitchValue = document.querySelector("#pitch-value");
+  const body = document.querySelector("body");
+
+  // init voices array
+  let voices = [];
+
+  const getVoices = () => {
+    voices = synth.getVoices();
+
+    // loop through voices and create an option for each one
+
+    voices.forEach((voice) => {
+      // create an option element
+      const option = document.createElement("option");
+      // fill the option with the voice and language
+      option.textContent = voice.name + "(" + voice.lang + ")";
+      // set needed option attributes
+      option.setAttribute("data-lang", voice.lang);
+      option.setAttribute("data-name", voice.name);
+      voiceSelect.appendChild(option);
+    });
+  };
+
+  getVoices();
+  if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = getVoices;
+  }
+
+  // speak
+  const speak = () => {
+    // check if speaking
+    if (synth.speaking) {
+      console.error("Already speaking...");
+      return;
+    }
+    if (textInput.value !== "") {
+      // add background animation gif
+      body.style.background = "#141414 url(./img/wave.gif)";
+      body.style.backgroundRepeat = "repeat-x";
+      body.style.backgroundSize = "100% 100%";
+      // get speak text
+      const speakText = new SpeechSynthesisUtterance(textInput.value);
+      // speak end
+      speakText.onend = (e) => {
+        console.log("Done speaking..");
+        body.style.background = "#141414";
+      };
+
+      // speak error
+      speakText.onerror = (e) => {
+        console.error("something went wrong!");
+      };
+
+      // selected voice
+      const selectedVoice =
+        voiceSelect.selectedOptions[0].getAttribute("data-name");
+
+      // loop through voices
+      voices.forEach((voice) => {
+        if (voice.name === selectedVoice) {
+          speakText.voice = voice;
+        }
+      });
+
+      // set pitch and rate
+      speakText.rate = rate.value;
+      speakText.pitch = pitch.value;
+
+      // speak!
+      synth.speak(speakText);
+    }
+  };
+
+  // event listeners
+
+  // text form submit
+  textForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    speak();
+    textInput.blur();
+  });
+
+  // rate value change
+  rate.addEventListener("change", (e) => (rateValue.textContent = rate.value));
+
+  // pitch value change
+  pitch.addEventListener(
+    "change",
+    (e) => (pitchValue.textContent = pitch.value)
+  );
+
+  // Voice select change
+  voiceSelect.addEventListener("change", (e) => speak());
+
   return (
     <div className="App">
       <div className="container text-center">
-        <img src="img/speech.png" className="mb-5" alt="power ding" />
+        <img src={logo} className="mb-5" alt="power ding" />
       </div>
       <div className="row">
         <div className="col-md-6 mx-auto">
