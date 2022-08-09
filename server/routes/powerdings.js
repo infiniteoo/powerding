52509e7baf08714b1ca25389a091511ebbe2fa93
Troy Dorman <troydorman@gmail.com>
2022-08-09 06:20:19 -0600

    successfully writing new powerdings to database

--- server/routes/powerdings.js
+++ server/routes/powerdings.js
@@ -4,15 +4,27 @@ const PowerDings = require("../database/models/powerdings");
 
 router.get("/", (req, res) => {
   console.log("powerding route hit");
+  PowerDings.find({ streamer: req.params.streamer })
+    .sort({ date: -1 })
+    .then((powerdings) => res.json(powerdings));
 });
 
 router.post("/", (req, res) => {
   // get all powerdings with streamer name
   console.log("powerding POST route hit");
-  console.log(req.body);
-  PowerDings.find({ streamer: req.params.streamer })
-    .sort({ date: -1 })
-    .then((powerdings) => res.json(powerdings));
+  const newPowerDing = new PowerDings({
+    streamer: req.body.streamer,
+    ttsVoice: req.body.voice,
+    senderName: req.body.donatorName,
+    mediaLink: req.body.mediaLink,
+    amountPaid: req.body.donationAmount,
+    played: false,
+    dateEntered: Date.now(),
+    message: req.body.text,
+    archived: false,
+  });
+
+  newPowerDing.save().then((powerding) => res.json(powerding));
 });
 
 module.exports = router;
