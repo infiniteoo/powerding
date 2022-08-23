bce9e1e3ffd30d82cd8dd736e8f089ac37ecf5b4
Troy Dorman <troydorman@gmail.com>
2022-08-23 04:00:02 -0600

    extract video id and time stamp from youtube link

--- src/components/StreamerAdmin/StreamerAdmin.jsx
+++ src/components/StreamerAdmin/StreamerAdmin.jsx
@@ -27,7 +27,35 @@ const StreamerAdmin = ({ userInfo, updateUser, loggedIn }) => {
       modestbranding: 1,
     },
   };
+  const extractVideoID = (url) => {
+    var regExp =
+      /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    if (match && match[2].length == 11) {
+      return match[2];
+    } else {
+      //error
+    }
+  };
 
+  const extractVideoTimeStamp = (url) => {
+    let regExp =
+      /^.*?(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*)(?:(\?t|&start)=(\d+))?.*/;
+
+    let match = url.match(regExp);
+    if (match) {
+      return match[4];
+    }
+  };
+
+  console.log(
+    "extracted video id",
+    extractVideoID("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+  );
+  console.log(
+    "extracted video id time stamp",
+    extractVideoTimeStamp("https://youtu.be/dQw4w9WgXcQ?t=143")
+  );
   return (
     <div className="dashboard_container">
       <div className="homeSplash">
