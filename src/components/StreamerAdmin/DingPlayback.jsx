f1dbfffc02b4c2eb4f8dccbd8b019167fb178b63
Troy Dorman <troydorman@gmail.com>
2022-08-27 07:14:19 -0600

    displaying animated gif in playback window

--- src/components/StreamerAdmin/DingPlayback.jsx
+++ src/components/StreamerAdmin/DingPlayback.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const DingPlayback = ({ dingPlaybackText }) => {
-  return <div>{dingPlaybackText}</div>;
+  return (
+    <div>
+      <iframe
+        src="https://giphy.com/embed/14SAx6S02Io1ThOlOY"
+        width="480"
+        height="270"
+        frameBorder={0}
+        className="giphy-embed"
+        allowFullScreen
+      ></iframe>
+
+      <p>
+        <a href="https://giphy.com/gifs/mostexpensivest-viceland-most-expensivest-14SAx6S02Io1ThOlOY"></a>
+      </p>
+      <h1>{dingPlaybackText}</h1>
+    </div>
+  );
 };
 
 export default DingPlayback;
