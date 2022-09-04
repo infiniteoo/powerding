49eee11f93ae21aecc2225161d9c8ac55974a9f5
Troy Dorman <troydorman@gmail.com>
2022-09-04 08:01:24 -0600

    generating 3 demo powerdings when a user signs up with random chuck norris quotes

--- server/config.js
+++ server/config.js
@@ -1,3 +1,13 @@
 exports.PORT = process.env.PORT || 8080;
 
 exports.CLIENT_ORIGIN = "http://powerding.com";
+
+exports.CHUCK_NORRIS_OPTIONS = {
+  method: "GET",
+  url: "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random",
+  headers: {
+    accept: "application/json",
+    "X-RapidAPI-Key": process.env.CHUCK_NORRIS_API_KEY,
+    "X-RapidAPI-Host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
+  },
+};
