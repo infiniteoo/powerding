0e445f255aa6b5109be69e171ca7c803a6d4c26b
Troy Dorman <troydorman@gmail.com>
2022-09-06 03:19:15 -0600

    added settings section to account dashboard

--- src/components/Dashboard.styled.js
+++ src/components/Dashboard.styled.js
@@ -9,3 +9,38 @@ export const AccountLabel = styled.div`
 export const AccountInfo = styled.div`
   color: white;
 `;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-between;
+  margin: 0 auto;
+  width: 100%;
+  /*  max-width: 1000px; */
+  padding: 0 1rem;
+  height: 100%;
+`;
+
+export const RightSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  width: 100%;
+  /* max-width: 1000px; */
+  padding: 0 1rem;
+  height: 100%;
+`;
+export const LeftSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  width: 100%;
+  /*  max-width: 1000px; */
+  padding: 0 1rem;
+  height: 100%;
+`;
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import ErrorSnackbar from "./Snackbar";
-import { AccountLabel, AccountInfo } from "./Dashboard.styled";
+import {
+  AccountLabel,
+  AccountInfo,
+  LeftSide,
+  RightSide,
+  Container,
+} from "./Dashboard.styled";
+
 import axios from "axios";
 
 const Dashboard = (props) => {
@@ -25,100 +32,127 @@ const Dashboard = (props) => {
     setSnackbarClose(false);
   };
 
+  const updateSettings = () => {
+    console.log("update settings");
+  };
+
   const customDonationURL = "/u/" + username;
   return (
     <div>
       <div className="dashboard_container">
         <div className="homeSplash">
-          <div>
-            <h1>account information</h1>
+          <Container>
+            <LeftSide>
+              <h1 className="dashboard_title">settings</h1>
+              <AccountLabel>Banner Image:</AccountLabel>
 
-            <AccountLabel>Username:</AccountLabel>
-            <AccountInfo> {username}</AccountInfo>
+              <AccountLabel>Intro Sound Effect:</AccountLabel>
 
-            <AccountLabel>Email Address:</AccountLabel>
-            <AccountInfo>{email}</AccountInfo>
-
-            <AccountLabel>Your Custom Donation URL:</AccountLabel>
-            <AccountInfo>
-              <a href={customDonationURL}>
-                http://www.powerding.com/{username}
-              </a>
-            </AccountInfo>
-
-            <AccountLabel>Last Login:</AccountLabel>
-            <AccountInfo>{formattedDate}</AccountInfo>
-
-            <AccountLabel>Access Level:</AccountLabel>
-            <AccountInfo>{accessLevel}</AccountInfo>
-
-            <AccountLabel>Confirmed: </AccountLabel>
-            <AccountInfo>{String(confirmed)}</AccountInfo>
-
-            <AccountLabel>
-              <h3>update password</h3>
-              <AccountInfo>Current Password:</AccountInfo>
+              <AccountLabel>Min. Amount for Media:</AccountLabel>
               <input
-                type="password"
-                id="currentPassword"
-                value={submittedPassword}
-                onChange={(e) => setSubmittedPassword(e.target.value)}
+                type="number"
+                id="minAmountForMedia"
+                /*  value={submittedPassword}
+                  onChange={(e) => setSubmittedPassword(e.target.value)} */
               />
-              <AccountInfo>New Password:</AccountInfo>
-              <input
-                type="password"
-                id="newPassword"
-                value={submittedNewPassword}
-                onChange={(e) => setSubmittedNewPassword(e.target.value)}
-              />
-              <AccountInfo>Confirm New Password:</AccountInfo>
-              <input
-                type="password"
-                id="confirmedNewPassword"
-                value={submittedConfirmPassword}
-                onChange={(e) => setSubmittedConfirmPassword(e.target.value)}
-              />
-            </AccountLabel>
-          </div>
 
-          <button
-            onClick={() => {
-              console.log("submittedPassword", submittedPassword);
-              console.log("submittedNewPassword", submittedNewPassword);
-              console.log("submittedConfirmPassword", submittedConfirmPassword);
+              <AccountLabel>Media Length (in seconds):</AccountLabel>
+              <input
+                type="number"
+                id="mediaLength"
+                /*  value={submittedPassword}
+                  onChange={(e) => setSubmittedPassword(e.target.value)} */
+              />
 
-              if (submittedNewPassword === submittedConfirmPassword) {
-                axios
-                  .post("/user/change-password", {
-                    password: submittedPassword,
-                    submittedNewPassword,
-                    submittedConfirmPassword,
-                    email,
-                  })
-                  .then((res) => {
-                    console.log(res);
-                    setSnackbarSeverity("success");
-                    setAlertMsg("Password updated successfully.");
-                    setSnackbarClose(true);
-                  })
-                  .catch((err) => {
+              <button onClick={updateSettings}>Update Settings</button>
+              <br />
+
+              <h1 className="dashboard_title">update password</h1>
+              <AccountLabel>
+                <AccountInfo>Current Password:</AccountInfo>
+                <input
+                  type="password"
+                  id="currentPassword"
+                  value={submittedPassword}
+                  onChange={(e) => setSubmittedPassword(e.target.value)}
+                />
+                <AccountInfo>New Password:</AccountInfo>
+                <input
+                  type="password"
+                  id="newPassword"
+                  value={submittedNewPassword}
+                  onChange={(e) => setSubmittedNewPassword(e.target.value)}
+                />
+                <AccountInfo>Confirm New Password:</AccountInfo>
+                <input
+                  type="password"
+                  id="confirmedNewPassword"
+                  value={submittedConfirmPassword}
+                  onChange={(e) => setSubmittedConfirmPassword(e.target.value)}
+                />
+              </AccountLabel>
+              <button
+                onClick={() => {
+                  if (submittedNewPassword === submittedConfirmPassword) {
+                    axios
+                      .post("/user/change-password", {
+                        password: submittedPassword,
+                        submittedNewPassword,
+                        submittedConfirmPassword,
+                        email,
+                      })
+                      .then((res) => {
+                        console.log(res);
+                        setSnackbarSeverity("success");
+                        setAlertMsg("Password updated successfully.");
+                        setSnackbarClose(true);
+                      })
+                      .catch((err) => {
+                        setSnackbarSeverity("error");
+                        setAlertMsg("Current password not correct.");
+                        setSnackbarClose(true);
+                      });
+                  } else {
                     setSnackbarSeverity("error");
-                    setAlertMsg("Current password not correct.");
+                    setAlertMsg("New passwords do not match.");
                     setSnackbarClose(true);
-                  });
-              } else {
-                setSnackbarSeverity("error");
-                setAlertMsg("New passwords do not match.");
-                setSnackbarClose(true);
-              }
+                  }
 
-              setSubmittedPassword("");
-              setSubmittedNewPassword("");
-              setSubmittedConfirmPassword("");
-            }}
-          >
-            Change Password
-          </button>
+                  setSubmittedPassword("");
+                  setSubmittedNewPassword("");
+                  setSubmittedConfirmPassword("");
+                }}
+              >
+                Change Password
+              </button>
+            </LeftSide>
+            <RightSide>
+              <h1>account information</h1>
+
+              <AccountLabel>Username:</AccountLabel>
+              <AccountInfo> {username}</AccountInfo>
+
+              <AccountLabel>Email Address:</AccountLabel>
+              <AccountInfo>{email}</AccountInfo>
+
+              <AccountLabel>Your Custom Donation URL:</AccountLabel>
+              <AccountInfo>
+                <a href={customDonationURL}>
+                  http://www.powerding.com/{username}
+                </a>
+              </AccountInfo>
+
+              <AccountLabel>Last Login:</AccountLabel>
+              <AccountInfo>{formattedDate}</AccountInfo>
+
+              <AccountLabel>Access Level:</AccountLabel>
+              <AccountInfo>{accessLevel}</AccountInfo>
+
+              <AccountLabel>Confirmed: </AccountLabel>
+              <AccountInfo>{String(confirmed)}</AccountInfo>
+            </RightSide>
+          </Container>
+
           {snackbarClose ? (
             <ErrorSnackbar
               severity={snackbarSeverity}
